1b6fb4fd9633600d18d339c0a704e7e06bf719ed
Flávio Bianchetti <fjb.developer@gmail.com>
2022-08-01 18:43:01 -0300

    feat:(src/pages/Cars.jsx) Exibir informações dos carros
    Detalhes:
    - incluídas rotinas para exibição das informações dos carros.

--- src/pages/Cars.jsx
+++ src/pages/Cars.jsx
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { queryData } from '../services/request';
 
 const Cars = () => {
+  const [listCars, setListCars] = useState([]);
+
+  useEffect(() => {
+    const data = {
+      "query": {
+        "selector": {
+          "@assetType": "car"
+        }
+      }
+    };
+    queryData('search', data)
+      .then((results) => {
+        setListCars(results);
+      }).catch((error) => {
+        console.error(error);
+        setListCars([]);
+      });
+  }, []);
+
+
   return (
-    <div>Lista de Carros</div>
+    <section>
+      <div>Lista de Carros</div>
+      {
+        listCars.map((car, index) => (
+          <div
+            key={ index }
+          >
+            <p>@key: <span>{ car['@key'] }</span> </p>
+            <p>@lastTouchBy: <span>{ car['@lastTouchBy'] }</span> </p>
+            <p>@lastTx: <span>{ car['@lastTx'] }</span> </p>
+            <p>driver.@key: <span>{ car.driver['@key'] }</span> </p>
+            <p>@id: <span>{ car['id'] }</span> </p>
+            <p>model: <span>{ car['model'] }</span> </p>
+          </div>
+        ))
+      }
+    </section>
   )
 }
 
