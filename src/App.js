8264f45ae835a501f034602d1a5e905c6effcd55
Flávio Bianchetti <fjb.developer@gmail.com>
2022-08-01 23:45:00 -0300

    feat:(src/pages/Cars.jsx) Alterada página de Carros - componente Table
    Detalhes:
    - Incluído componente Table na página de carros.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Cars from './pages/Cars';
 function App() {
   return (
     <Routes>
-      <Route path="/" element={ <Cars /> } />
+      <Route path="/cars" element={ <Cars /> } />
     </Routes>
   );
 }
--- src/pages/Cars.jsx
+++ src/pages/Cars.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { queryData } from '../services/request';
+import Table from '../components/Table';
 
 const Cars = () => {
   const [listCars, setListCars] = useState([]);
@@ -14,17 +15,40 @@ const Cars = () => {
     };
     queryData('search', data)
       .then((results) => {
-        setListCars(results);
+        const data = results.map((elem) => {
+          return {
+            '@key': elem['@key'],
+            '@lastTouchBy': elem['@lastTouchBy'],
+            '@lastTx': elem['@lastTx'],
+            'driver.@key': elem.driver['@key'],
+            '@id': elem['id'],
+            'model': elem['model'],
+          };
+        });
+        setListCars(data);
       }).catch((error) => {
         console.error(error);
         setListCars([]);
       });
   }, []);
 
+  const header = [
+    '@Key',
+    '@lastTouchBy',
+    '@lastTx',
+    'driver.@key',
+    'id',
+    'model',
+    'ação',
+  ];
+
+  const handleClickEdit = () => true;
+
+  const handleClickDelete = () => true;
 
   return (
     <section>
-      <div>Lista de Carros</div>
+      {/* <div>Lista de Carros</div>
       {
         listCars.map((car, index) => (
           <div
@@ -38,6 +62,16 @@ const Cars = () => {
             <p>model: <span>{ car['model'] }</span> </p>
           </div>
         ))
+      } */}
+      {
+        Table({
+          'title': 'Lista de Carros',
+          'header': header,
+          'data': listCars,
+          'handleClickEdit': handleClickEdit,
+          'handleClickDelete': handleClickDelete,
+          'isIconsDisabled': false,
+        })
       }
     </section>
   )
