712cc382c0a87103730e00a1f30b6a710cba7410
Flávio Bianchetti <fjb.developer@gmail.com>
2022-08-05 01:47:40 -0300

    feat:(src/context/RacesProvider.jsx) Incluídas configurações de query
    Detalhes:
    - incluídas constantes com querys para gerenciamento do CRUD da lista
      de carros.

--- src/context/RacesProvider.jsx
+++ src/context/RacesProvider.jsx
@@ -4,6 +4,82 @@ import { queryData, invokeData } from '../services/request';
 import PropTypes from 'prop-types';
 
 const RacesProvider = ({ children }) => {
+
+  const queryCar = () => ({
+    "query": {
+      "selector": {
+        "@assetType": "car"
+      }
+    }
+  });
+
+  const queryDriver = (asset, keyDriver) => ({
+    "query": {
+      "selector": {
+        "@assetType": asset,
+        "@key": keyDriver,
+      }
+    }
+  });
+
+  const queryTeam = (keyTeam) => ({
+    "query": {
+      "selector": {
+        "@assetType": "team",
+        "@key": keyTeam,
+      }
+    }
+  });
+
+  const queryFindCar = (id) => {
+    return {
+      "query": {
+        "selector": {
+          "@assetType": "car",
+          "id": id
+        }
+      }
+    };
+  };
+
+  const queryPostCar = ({ idCar, model, idDriver }) => {
+    return {
+      "asset": [
+        {
+          "@assetType": "car",
+          "id": idCar,
+          "model": model,
+          "driver": {
+            "@assetType": "driver",
+            "id": idDriver
+          }
+        }
+      ]
+    };
+  };
+
+  const queryPutCar = ({ idCar, model, idDriver }) => {
+    return {
+      "update": {
+        "@assetType": "car",
+        "id": idCar,
+        "model": model,
+        "driver": {
+          "@assetType": "driver",
+          "id": idDriver
+        }
+      }
+    };
+  };
+
+  const queryDelete = (asset, id) => {
+    return {
+      "key": {
+        "@assetType": asset,
+        "id": id
+      }
+    };
+  }
   const listRacesProvider = {
   };
 
