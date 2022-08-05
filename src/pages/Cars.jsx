5ec293e182ac8152f796ffbac44249f41dabbefd
Flávio Bianchetti <fjb.developer@gmail.com>
2022-08-05 02:11:47 -0300

    refactor:(src/pages/Cars.jsx) Rotinas movidas para o provider
    Detalhes:
    - Removidas rotinas locais para dentro do arquivo do provider.

--- src/pages/Cars.jsx
+++ src/pages/Cars.jsx
@@ -1,102 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import { queryData } from '../services/request';
+import React from 'react';
 import Table from '../components/Table';
+import Form from '../components/Form';
 
 const Cars = () => {
-  const [listCars, setListCars] = useState([]);
-
-  const queryCar = () => ({
-    "query": {
-      "selector": {
-        "@assetType": "car"
-      }
-    }
-  });
-
-  const queryDriver = (keyDriver) => ({
-    "query": {
-      "selector": {
-        "@assetType": "driver",
-        "@key": keyDriver,
-      }
-    }
-  });
-
-  const queryTeam = (keyTeam) => ({
-    "query": {
-      "selector": {
-        "@assetType": "team",
-        "@key": keyTeam,
-      }
-    }
-  });
-
-  useEffect(() => {
-    queryData('search', queryCar())
-      .then((resultsCar) => {
-        const dataCar = resultsCar.map((elem) => {
-          const result = {
-            '@key': elem['@key'],
-            'id': elem['id'],
-            'model': elem['model'],
-            'driver': '',
-            'team': ' - ',
-          };
-
-          if (elem.driver["@key"].split(':')[0] === 'car') {
-            result['driver'] = elem['model'];
-          } else {
-          queryData('search', queryDriver(elem.driver['@key']))
-            .then((resultsDriver) =>{
-              result['driver'] = resultsDriver[0].name;
-              queryData('search', queryTeam(resultsDriver[0].team['@key']))
-                .then((resultsTeam) => {
-                  result['team'] = resultsTeam[0].name;
-                }).catch((error) => {
-                  console.error(error);
-                  result['team'] = 'Not found'; 
-                });
-            }).catch((error) => {
-              console.error(error);
-              result['driver'] = 'Not found';
-              result['team'] = 'Not found';
-            });
-          }
-          return result;
-
-        });
-        setListCars(dataCar);
-      }).catch((error) => {
-        console.error(error);
-        setListCars([]);
-      });
-  }, []);
-
-  const headers = [
-    '@Key',
-    'id',
-    'model',
-    'driver',
-    'team',
-    'ações',
-  ];
-
-  const handleClickEdit = () => true;
-
-  const handleClickDelete = () => true;
-
   return (
     <section>
-      <Table
-        title={ 'Lista de Carros' }
-        header={ headers }
-        data={ listCars }
-        handleClickEdit={ handleClickEdit }
-        handleClickDelete={ handleClickDelete }
-        isIconsDisabled={ false }
-      />
+      <Form />
+      <Table />
     </section>
   )
-}
+};
 
 export default Cars;
